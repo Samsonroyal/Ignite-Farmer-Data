6935e044c139e3252008a644b61ea5b382bae160
Were Samson Bruno <werefast2000@gmail.com>
2023-11-20 13:41:58 +0000

    defined useRef correctly

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Chart from 'chart.js';
+import React, { useState, useEffect,  useRef } from 'react';
+import * as Chart from 'chart.js';
 import '../styles/Card.css';
 
 const WeatherCard = () => {
