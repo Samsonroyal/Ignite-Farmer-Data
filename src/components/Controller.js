d1c3fe01df55ad44caf9cb1993ae5682cc614510
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 16:00:04 +0000

    modularised controller card

---
+++ src/components/Controller.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="overlap-group">
+            <div className="ellipse-2" />
+            <div className="ellipse-3" />
+            <div className="ellipse-4" />
+            <div className="text-wrapper-4">0mm</div>
+            <div className="text-wrapper-5">Rainfall</div>
+            <div className="ellipse-5" />
+            <div className="text-wrapper-6">0mm</div>
+            <div className="ellipse-6" />
+            <div className="ellipse-7" />
+            <div className="text-wrapper-7">Wind</div>
+            <div className="text-wrapper-8">0mm</div>
+            <div className="text-wrapper-9">Rain</div>
+            <div className="text-wrapper-10">Wind</div>
+            <div className="text-wrapper-11">0mm</div>        </div>
+
+    );
+};
+
+export default Card;
