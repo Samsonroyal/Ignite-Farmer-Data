6685a68314209907ba19b783b8a00840a42d00ea
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 16:00:16 +0000

    created sidenav module

---
+++ src/components/SideNav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div className="overlap-group-2">
+            <div className="overlap-7">
+                <div className="text-wrapper-33">Farmers</div>
+                <img className="vector-2" alt="Vector" src="vector.svg" />
+            </div>
+            <div className="overlap-8">
+                <div className="text-wrapper-34">USSD</div>
+                <img className="vector-3" alt="Vector" src="vector-3.svg" />
+            </div>
+            <div className="overlap-9">
+                <div className="text-wrapper-34">Sensors</div>
+                <img className="vector-4" alt="Vector" src="vector-2.svg" />
+            </div>
+            <div className="overlap-10">
+                <div className="text-wrapper-35">Revenue</div>
+                <img className="vector-5" alt="Vector" src="image.svg" />
+            </div>        </div>
+
+    );
+}
+
+export default SideNav;
