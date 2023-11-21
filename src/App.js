8909a0ce123bc7ae6113bcfb0d76e399a23d37d4
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 11:40:31 +0000

    add new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WeatherStats from './components/WeatherStatistics';
+import MyWeatherComponent from './components/Weather'
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<WeatherStats />} />
+        <Route path="/weather" element={<MyWeatherComponent />} />
       </Routes>
     </Router>
   );
