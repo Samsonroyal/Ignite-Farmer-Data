94c43a8d3b59de8ee347f7e9e8bf8d7486498eb2
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 14:53:20 +0000

    paste exported figma code into dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "../App.css";
+
+export const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="overlap">
+        <img className="ellipse" alt="Ellipse" src="../Ellipse 11.png" />
+        <div className="text-wrapper">Ignite Dashboard</div>
+        <div className="group">
+          <div className="div" />
+        </div>
+        <div className="text-wrapper-2">Welcome back Kwame.</div>
+      </div>
+      <div className="text-wrapper-3">23 October 2023</div>
+      <div className="overlap-group">
+        <div className="ellipse-2" />
+        <div className="ellipse-3" />
+        <div className="ellipse-4" />
+        <div className="text-wrapper-4">0mm</div>
+        <div className="text-wrapper-5">Rainfall</div>
+        <div className="ellipse-5" />
+        <div className="text-wrapper-6">0mm</div>
+        <div className="ellipse-6" />
+        <div className="ellipse-7" />
+        <div className="text-wrapper-7">Wind</div>
+        <div className="text-wrapper-8">0mm</div>
+        <div className="text-wrapper-9">Rain</div>
+        <div className="text-wrapper-10">Wind</div>
+        <div className="text-wrapper-11">0mm</div>
+      </div>
+      <div className="overlap-2">
+        <div className="rectangle" />
+        <div className="text-wrapper-12">Plant Activity</div>
+        <div className="rectangle-2" />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="rectangle-5" />
+        <div className="rectangle-6" />
+        <div className="text-wrapper-13">Weekly</div>
+        <img className="img" alt="Ellipse" src="ellipse-12.svg" />
+        <img className="ellipse-8" alt="Ellipse" src="ellipse-13.svg" />
+        <img className="ellipse-9" alt="Ellipse" src="ellipse-14.svg" />
+        <div className="text-wrapper-14">Final growth</div>
+        <div className="text-wrapper-15">Vegetation</div>
+        <div className="text-wrapper-16">Seed Phase</div>
+        <div className="text-wrapper-17">week 1</div>
+        <div className="text-wrapper-18">week 2</div>
+        <div className="text-wrapper-19">week 3</div>
+        <img className="line" alt="Line" src="line-1.svg" />
+        <img className="line-2" alt="Line" src="line-2.svg" />
+        <img className="line-3" alt="Line" src="line-3.svg" />
+      </div>
+      <div className="overlap-3">
+        <div className="text-wrapper-20">Water Levels</div>
+        <div className="text-wrapper-21">Tomato</div>
+        <div className="overlap-4">
+          <div className="overlap-5">
+            <img className="mark-area" alt="Mark area" src="mark-area.svg" />
+            <img className="line-4" alt="Line" src="line-4.svg" />
+            <img className="line-5" alt="Line" src="line-5.svg" />
+            <img className="line-6" alt="Line" src="line-6.svg" />
+            <img className="line-7" alt="Line" src="line-7.svg" />
+            <div className="text-wrapper-22">850</div>
+          </div>
+          <p className="element">
+            <span className="span">0</span>
+            <span className="text-wrapper-23">mm</span>
+          </p>
+          <div className="text-wrapper-24">250</div>
+          <div className="text-wrapper-25">650</div>
+        </div>
+        <div className="text-wrapper-26">Jan</div>
+        <div className="text-wrapper-27">Feb</div>
+        <div className="text-wrapper-28">Mar</div>
+        <div className="text-wrapper-29">Jul</div>
+        <div className="text-wrapper-30">Jun</div>
+      </div>
+      <img className="vector" alt="Vector" src="vector-4.svg" />
+      <img className="planner" alt="Planner" src="planner.png" />
+      <div className="div-wrapper">
+        <div className="text-wrapper-31">Alert</div>
+      </div>
+      <div className="overlap-6">
+        <div className="text-wrapper-32">Send</div>
+      </div>
+      <div className="overlap-group-2">
+        <div className="overlap-7">
+          <div className="text-wrapper-33">Dashboard</div>
+          <img className="vector-2" alt="Vector" src="vector.svg" />
+        </div>
+        <div className="overlap-8">
+          <div className="text-wrapper-34">USSD</div>
+          <img className="vector-3" alt="Vector" src="vector-3.svg" />
+        </div>
+        <div className="overlap-9">
+          <div className="text-wrapper-34">Sensors</div>
+          <img className="vector-4" alt="Vector" src="vector-2.svg" />
+        </div>
+        <div className="overlap-10">
+          <div className="text-wrapper-35">Revenue</div>
+          <img className="vector-5" alt="Vector" src="image.svg" />
+        </div>
+      </div>
+    </div>
+  );
+};
