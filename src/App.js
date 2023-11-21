9f98991dbfa811a028db8f6fa399a7f346ceeada
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 14:54:20 +0000

    add dashboard route to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WeatherStats from './components/WeatherStatistics';
 import MyWeatherComponent from './components/Weather'
+import { Dashboard } from './pages/Dashboard';
 import './App.css';
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<WeatherStats />} />
         <Route path="/weather" element={<MyWeatherComponent />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        
+        
       </Routes>
     </Router>
   );
