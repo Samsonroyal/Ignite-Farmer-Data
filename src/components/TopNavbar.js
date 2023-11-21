a912cb0e4045fe31729839410c0283a7912763e2
Were Samson Bruno <werefast2000@gmail.com>
2023-11-21 16:00:27 +0000

    created topnav module

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TopNavbar = () => {
+    return (
+        <div className="overlap">
+            <img className="ellipse" alt="Ellipse" src="../Ellipse 11.png" />
+            <div className="text-wrapper">Ignite Dashboard</div>
+            <div className="group">
+                <div className="div" />
+            </div>
+            <div className="text-wrapper-2">Welcome back Kwame.</div>        </div>
+
+    );
+};
+
+export default TopNavbar;
