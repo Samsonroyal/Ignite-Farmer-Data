2849923e771e0b281c36dc132a9b7ebb99a6cf34
perismuthoni <muthonimuriuki22@gmail.com>
2023-11-22 10:05:11 +0000

    progressive

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -5,10 +5,10 @@ const TopNavbar = () => {
         <div className="navbar">
             <image className="ellipse" alt="Ellipse" src="../Ellipse 11.png" />
             <div className="navbar-title">Ignite Dashboard</div>
-            <div className="wrapper">
-                <div className="">
+            <div className="searchbar-holder">
+               
                     <input className="searchbar" type="text" placeholder="Search" />
-                </div>
+               
             </div>
             <div className="navbar-welcome">Welcome back Kwame.
             <span role="img" alt="hello">
