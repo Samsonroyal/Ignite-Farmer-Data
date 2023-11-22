95b07e544c8b283876b44bc323826df3ab1405d6
Were Samson Bruno <werefast2000@gmail.com>
2023-11-22 01:17:07 +0000

    renamed classnames in navbar and refactored code

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -5,8 +5,11 @@ const TopNavbar = () => {
         <div className="overlap">
             <img className="ellipse" alt="Ellipse" src="../Ellipse 11.png" />
             <div className="text-wrapper">Ignite Dashboard</div>
-            <div className="group">
-                <div className="div" />
+            <div className="wrapper">
+                <div className="overlap-group1">
+                    <img className="vector" alt="Vector" src="../vector-3.svg" />
+                    <input className="searchbar" type="text" placeholder="Search" />
+                </div>
             </div>
             <div className="text-wrapper-2">Welcome back Kwame.</div>        </div>
 
