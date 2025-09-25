d2e9f7b29ff6d75c8697aa3c52a4523741b5e4ee
Were Samson Bruno <werefast2000@gmail.com>
2025-09-25 12:16:54 +0000

    Refactor SideNav component to use MUI Drawer and List

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,39 +1,53 @@
 import React from 'react';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Box,
+  Paper,
+} from '@mui/material';
 
 const SideNav = () => {
+  const items = ['Users', 'USSD', 'Revenue', 'Sensors'];
 
-
-    return (
-        <div className="side-nav">
-           
-
-            <div className="button-holder">
-                <button className="overlap">
-                    Users
-                </button>
-            </div>
-
-
-            <div className="button-holder">
-                <button className="overlap">
-                    USSD
-                </button>
-            </div>
-            <div className="button-holder">
-                <button className="overlap">
-                  Revenue
-                    
-                </button>
-            </div>
-            <div className="button-holder">
-                <button className="overlap">
-                  Sensors
-                    
-                </button>
-            </div>
-        </div>
-    );
-}
-
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: 220,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: 220,
+          boxSizing: 'border-box',
+          p: 2,
+          bgcolor: '#fff',
+          boxShadow: '2px 0 8px rgba(0,0,0,0.05)',
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        <List>
+          {items.map((text) => (
+            <Paper
+              key={text}
+              elevation={2}
+              sx={{ mb: 2, borderRadius: 2, overflow: 'hidden' }}
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            </Paper>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
 
 export default SideNav;
